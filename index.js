ad47b1507961d60dfcc4a029ba335ed06a903b25
Antonio <isc20350280@gmail.com>
2023-09-22 09:49:06 -0600

    Query params

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 
-app.get("/:user/:password",(req,res) => {
+app.get("/:user/:password",(req,res) => { //Endpoint
     const {user,password} = req.params
     if(user == 'antonio' && password == '123'){
         res.json({msg:'Inicio de sesion exitoso'})
@@ -10,6 +10,20 @@ app.get("/:user/:password",(req,res) => {
     }
         res.json({msg:'Fallo en el usuario y contraseña'})
     })
+
+//http://localhost:3000/login?user=antonio&password=123
+app.get('/login',(req, res) =>{
+    const {user,password} = req.query
+    if(!user || !password){
+        res.status(400).json({msg:'You need to provide <user> and <password> paramas'})
+    }
+
+    if(user == 'antonio' && password == '123'){
+        res.json({msg:'sign In succesfully'})
+        return
+    }
+    res.status(404).json({msg:'wrong user or password'})
+    })  
 
 app.post("/",(req,res) => {
     res.json({
