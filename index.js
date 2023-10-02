82cb6012136126305aafdd9bedb349a32d38fbb0
Antonio <isc20350280@gmail.com>
2023-10-02 13:17:11 -0600

    Query params

--- index.js
+++ index.js
@@ -1,73 +1,55 @@
-const express = require("express")
-
+const express = require ('express')
 const app = express()
 
 app.use(express.json())
 
-app.get("/:user/:password",(req,res) => { //Endpoint
-    const {user,password} = req.params
-    if(user == 'antonio' && password == '123'){
-        res.json({msg:'Inicio de sesion exitoso'})
+// http://localhost:3000/hernandez/123
+app.get('/:user/:password', (req, res) =>{ //Endponit
+    const {user, password} = req.params
+    if (user == 'antonio' && password == '123'){
+        res.json({msg:'inicio sesion exitoso'})
         return
     }
-        res.json({msg:'Fallo en el usuario y contraseña'})
-    })
-
-//http://localhost:3000/login?user=antonio&password=123
-app.get('/login',(req, res) =>{
-    const {user,password} = req.query
-    if(!user || !password){
-        res.status(400).json({msg:'You need to provide <user> and <password> paramas'})
+    res. json({msg:'fallo contraseña invalida'})
+})
+//http://localhost:3000/login?user=hernandez%password=123
+app.get('/login', (req,res) => {
+    const {user, password} = req.query
+    if (!user || !password) {
+        res.status(400).json({msg:'you need to provide <user> and <password> params'})
     }
-
-    if(user == 'antonio' && password == '123'){
-        res.json({msg:'sign In succesfully'})
-        return
-    }
-    res.status(404).json({msg:'wrong user or password'})
-    })  
-
-app.post('/login',(req, res)=>{
-    const body = req.body
-
-    res.json(body)
-})
-
-app.post("/",(req,res) => {
-    res.json({
-        msg:'Hello Post'
-    })
-})
-
-
-app.put("/",(req,res) =>{
-    res.json({
-        msg:'Hello put'
-    })
-})
-
-app.patch("/",(req,res) =>{
-    res.json({
-        msg:'Hello PATCH'
-    })
-})
-
-app.delete("/",(req,res) =>{
-    res.json({
-        msg:'Hello DELETE'
-    })
-})
-
+    
+    if (user === 'antonio' && password === '123'){
+        res.json(404).json({msg:'wrong user or password'})
+    
+}})
+///02/10/2023 ////  //usos de seccion con postman desde enviar datos y modificar datos///
 app.post('/login' , (req,res) => {
     const  {user, password}= req.body
     if (user == 'antonio' && password == '123'){
-        res.json({msg:'inicio seccion exitoso'})
+        res.json({msg:'inicio sesion exitoso'})
         return
     }
     res. json({msg:'fallo contraseña invalida'})
     
 })
 
-app.listen(3000,() =>{
-    console.log("listening on port 3000")
+app.put('/', (request, response) =>{
+    response.json({msg:'Hola PUT'})
+})
+app.patch('/', (request, response) =>{
+    response.json({msg:'Hola PATCH'})
+})
+app.delete('/', (request, response) =>{
+    response.json({msg:'Hola DELETE'})
+})
+
+/**
+ * FUNCIONALIDAD
+ * 
+ */
+
+//http://localhost:3000/
+app.listen(3000, () => {
+    console.log('listening on port 3000')
 })
